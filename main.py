3616d6fd99a93e00f262b85e8a5b8379d69dbd2d
Hjalte Nielsen <hn@m-tech.aau.dk>
2019-08-21 10:48:12 +0200

    implementing a config file for the pins on the pi

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from pick_box import PickBox
 from time import sleep
+import yaml
 
+with open('pin_config.yaml','r') as yamlfile:
+    cfg = yaml.load(yamlfile)
 
-my_box = PickBox(pir_pin = 17, led_pin = 18)
-sleep(10)
+for box, pins in cfg.:
+    print(box, pins)
+
+#my_box = PickBox(pir_pin = 17, led_pin = 18)
+#my_box.select()
+
