2d674d74c595fec97388170ba50ec08e33a08c28
Hjalte Nielsen <hn@m-tech.aau.dk>
2019-10-09 14:16:20 +0200

    FIX: phrasing config files fixed

--- main.py
+++ main.py
@@ -4,31 +4,38 @@ import yaml
 import warnings
 from packml import PackML
 
-pin_config = {}
-boxes = {}
-packml = PackML()
+
 
 def read_config(pin_path = 'pin_config.yaml', box_path = 'box_config.yaml'):
+    # return list of boxes 
+    boxes_to_return = {}
+
     try:
         # load the pin config file
         with open(pin_path ,'r') as yamlfile:
             pin_config = yaml.load(yamlfile)         
     except FileNotFoundError as f:
-        warning.warn('The box configuration file: %s, could not be found, default values are used' %f)
+        raise('The box configuration file: %s, could not be found' %f)
 
     # loop throug the pin configurations for each connector
     for connector, args in pin_config.items():
-        # Make sure the bothe pins are specified.
-        if all(k in args for k in ('led_pin', 'pir_pin')):
-            # Copy the config to global dict.
-            pin_config[connector] = args
         
-        # Give a warning about missing config params. 
+        # Make sure the bothe pins are specified.
+        # If they are defined do nothing
+        if all(k in args for k in ('led_pin', 'pir_pin')):
+            pass
+        # Else give a warning about missing config params. 
         elif 'led_pin' not in args:
-            warnings.warn('The config id "%s" does not have the mandatory key "led_pin". The entry is skiped' %connector)
-        else:
-            warnings.warn('The config id "%s" does not have the mandatory key "pir_pin". The entry is skiped' %connector)
-    
+            warnings.warn('The connector config id "%s" does not have the mandatory key "led_pin". The entry is skiped' %connector)
+            pin_config.pop(connector, None)
+        elif 'pir_pin' not in args:
+            warnings.warn('The connector config id "%s" does not have the mandatory key "pir_pin". ' %connector)
+            pin_config.pop(connector, None)
+        else: 
+            warnings.warn('something went wrong in reading the pin config for connector id "%s". The entry is skiped' %connector)
+            pin_config.pop(connector, None)
+
+    # Try to load the box configuration. 
     try:
         # Load the box configuration 
         with open(box_path ,'r') as yamlfile:
@@ -36,68 +43,53 @@ def read_config(pin_path = 'pin_config.yaml', box_path = 'box_config.yaml'):
         
         # Go throug the config box by box
         for box_id, args in box_config.items():
-            # Fetch the pins
-            pir_pin = pin_config[box_id]['pir_pin']
-            led_pin = pin_config[box_id]['led_pin']
-            
+            # Try to fetch the pins
+            try:
+                pir_pin = pin_config[box_id]['pir_pin']
+                led_pin = pin_config[box_id]['led_pin']
+            except KeyError:
+                warnings.warn('pin config for box id %s was not loaded. Are you sure the pin config is formatted correctly?' %box_id)
+                
+                # Go back to top and read next box config
+                continue
+
             # Chechk for box name and content config
             if 'box_name' in args:
                 box_name = args['box_name']
+            else: box_name = None
+
             if 'content_count' in args:
                 content_count = args['content_count']
+            else: content_count = None
+
             if 'content_id' in args:
                 content_id = args['content_id']
+            else: content_id = None
+
             # Finally create the instance of the pick by light box
-            new_box = PickBox(packml, pir_pin,led_pin, box_id, box_name, content_id, content_count)
+            new_box = PickBox(pir_pin,led_pin, box_id, box_name, content_id, content_count)
 
             # Push the newly created box the the list of boxes. 
-            boxes[box_id] = new_box
+            boxes_to_return[box_id] = new_box        
+        return boxes_to_return
 
     except FileNotFoundError as f:
         raise ('The box configuration file: %s, could not be found, any new pick by light boxes must be configured at run time' %f)
-
-
-
-
-
-read_config()
-print(boxes)
-packml.start_server()
-
-# for key , box in boxes.items():
-#     print(box.get_content())
-#     box.select()
-#     sleep(8)
-
-import sys, time
-
-x = 1
-# while True:
-#     try:
-#         selected = input('select 1-3 box')
-#         tmp = boxes[int(selected)]
-#         tmp.select()
-
-#     except KeyboardInterrupt:
-#         print ("Bye")
-#         packml.server.stop()
-#         sys.exit()
-
-import random 
-
-next_time = time.time()
-while True: 
     
-    try:
-        if time.time() >= next_time:
-            tmp = boxes[random.randint(1,4)]
-            if tmp.select():
-                next_time = time.time() + random.randint(7,10)
+    # finally return the list of boxes. 
+    return boxes_to_return
 
-    except KeyboardInterrupt:
-        print ("Bye")
-        packml.server.stop()
-        sys.exit()
 
+
+
+if __name__ == "__main__":
+    
+
+    #packml = PackML()
+
+    boxes = read_config()
+    print(boxes)
+
+    #packml.start_server()
 
 
