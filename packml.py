8d7a5b69cb85d532bd70ecc713f13db89803009c
Hjalte Nielsen <hn@m-tech.aau.dk>
2019-10-16 11:11:40 +0200

    work in progress

--- packml.py
+++ packml.py
@@ -1,31 +1,132 @@
 from opcua import ua, Server
+from opcua.common.type_dictionary_buider import DataTypeDictionaryBuilder, get_ua_class
 import time
 
-server = Server('./opcua_cache')
 
-server.set_endpoint('opc.tcp://0.0.0.0:4840/UA/PickByLight')
-server.set_server_name("Pick By Light Server")
+class PackMLServer:
+
+    def __init__(self):
+        self.server = Server('./opcua_cache')
+
+        self.server.set_endpoint('opc.tcp://0.0.0.0:4840/UA/PickByLight')
+        self.server.set_server_name("Pick By Light Server")
+
+        # idx name will be used later for creating the xml used in data type dictionary
+        # setup our own namespace, not really necessary but should as spec
+        self._idx_name = 'http://examples.freeopcua.github.io'
+        self.idx = self.server.register_namespace(self._idx_name)
+        #set all possible endpoint policies for clienst to connect through
+        self.server.set_security_policy([
+            ua.SecurityPolicyType.NoSecurity,
+            ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt,
+            ua.SecurityPolicyType.Basic256Sha256_Sign]
+        )
+        self.dict_builder = DataTypeDictionaryBuilder(self.server, self.idx, self._idx_name, 'PackMLBaseObjectType')
+
+
+        # get Objects node, this is where we should put our custom stuff
+        objects = self.server.get_objects_node()
+
+        types = self.server.get_node(ua.ObjectIds.BaseObjectType)
+
+        self.PackMLBaseObjectType = types.add_object_type(self.idx, "PackMLBaseObjectType")
+        self.Admin = objects.add_object(self.idx, "Admin", self.PackMLBaseObjectType.nodeid)
+        self.Status = objects.add_object(self.idx, "Status", self.PackMLBaseObjectType.nodeid)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+    #quit()
+
+    def start_server(self):
+        self.server.start()
+
+    def create_structure(self, name):
+        # save the created data type
+        return self.dict_builder.create_data_type(name)
+
+    def complete_creation(self):
+        self.dict_builder.set_dict_byte_string()
+    
+    def create_object(self, name):
+        return self.server.nodes.base_object_type.add_object(self.idx, name)
+
+
+
+# This set up the server defined just above. 
+with PackMLServer() as ua_server:
+    
+    # add one basic structure. This should probaly be moved the the packml server class. 
+    basic_struct_name = 'basic_parameter'
+    basic_struct = ua_server.create_structure(basic_struct_name)
+    basic_struct.add_field('ID', ua.VariantType.Int32)
+    basic_struct.add_field('Name', ua.VariantType.String)
+    basic_struct.add_field('Unit', ua.VariantType.String)
+    basic_struct.add_field('Value', ua.VariantType.Int32)
+
+    # this operation will write the OPC dict string to our new data type dictionary
+    # namely the 'MyDictionary'
+    ua_server.complete_creation()
+    
+    # create new nodes
+    # status_node = ua_server.server.nodes.base_object_type.add_object(idx, 'Status').set_modelling_rule(True)
+    # admin_node = ua_server.server.nodes.base_object_type.add_object(idx, 'Admin').set_modelling_rule(True)
+
+    
+    # get the working classes
+    ua_server.server.load_type_definitions()
+
+    # Create one test structure
+    basic_var = ua_server.server.nodes.objects.add_variable(ua.NodeId(namespaceidx=ua_server.idx), 'BasicStruct',
+                                                            ua.Variant(None, ua.VariantType.Null),
+                                                            datatype=basic_struct.data_type)
+
+    basic_var.set_writable()
+    basic_msg = get_ua_class(basic_struct_name)()
+    basic_msg.ID = 3
+    basic_msg.Gender = True
+    basic_msg.Comments = 'Test string'
+    basic_var.set_value(basic_msg)
+
+
+  
+
+    ua_server.start_server()
+
+
+# class that defines a PackML parameter
+# class Param:
+#     def __init__(self,id,name,unit,value = None):
+#         self.id = id
+#         self.name = name
+#         self.unit = unit
+#         self.value = value
+
+
         
-# setup our own namespace, not really necessary but should as spec
-uri = "http://examples.freeopcua.github.io"
-idx = server.register_namespace(uri)
-
-
-#set all possible endpoint policies for clienst to connect through
-server.set_security_policy([
-    ua.SecurityPolicyType.NoSecurity,
-    ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt,
-    ua.SecurityPolicyType.Basic256Sha256_Sign])
-
-# create new nodes
-command_node = server.nodes.base_object_type.add_object(idx, 'Command')
-command_node.add_variable(idx, 'testing_this_node',1).set_modelling_rule(True)
-command_node.add_property(idx, 'a property test','the value')
-objects = server.get_objects_node()
 
 
 
-server.start()        
+
+
+
+# picked_boxes = Param(1234,"test","list",[5,6,7])
+# status_node.add_variable(idx, 'picked_boxes', picked_boxes).set_modelling_rule(True)
+
+# command_node.add_property(idx, 'a property test','the value')
+# objects = server.get_objects_node()
+
+
+
+# server.start()        
  
 
-      
+    
+try:
+    while True:
+        time.sleep(1)
+finally:
+    # close connection, remove subcsriptions, etc
+    server.stop()
