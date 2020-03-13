4d6222c0add4bcda6bc8e1d4aa9f7abcc2fff11e
Hjalte Nielsen <hjalte33@gmail.com>
2020-03-13 11:29:26 +0100

    Major overhaul. It is working now.

--- logic/rack_controller.py
+++ logic/rack_controller.py
@@ -4,6 +4,25 @@ from threading import Thread
 from datetime import datetime, timedelta
 from itertools import zip_longest
 from time import sleep
+from collections import defaultdict
+from typing import Callable
+
+class PortState:
+    def __init__(self, selected = False,
+                       amount = 0,
+                       callback : Callable[[int], None] = None, #dono if this works.
+                       auto_light_off = True,
+                ):
+        self.selected = selected
+        self.amount = amount
+        self.callback = callback
+        self.auto_light_off =  auto_light_off
+        self.select_timestamp = datetime.fromtimestamp(0) # just some time long ago. 
+        self.pick_timestamp = datetime.fromtimestamp(0)# just some time long ago
+    
+    def update(self, **kwargs):
+        for arg, value in kwargs.items():
+            setattr(self,arg,value)
 
 
 class RackController(Thread):
@@ -11,12 +30,15 @@ class RackController(Thread):
         super().__init__(daemon = True)
 
         ports = self._port_sterilizer(ports)
-        # create dict for the rack. Zip_longest pads with None if there's no boxes in the list of boxes.
+        # create dict of all the port objects for the rack.
         self.ports = {port.port_number : port for port in ports}
+        # Create a dict of all box objects. Zip_longest pads with None if there's no boxes in the input list of boxes.
         self.boxes = {port.port_number : box for port, box in zip_longest(ports,boxes)}
-        self.selected_ports = {}
+        # Initialize the state of all boxes with default values. 
+        self.ports_select_state = {port_number : PortState() for port_number in self.ports.keys()}
         self.light_last_cycle = datetime.now()
-        self.light_state = False
+        self.rack_light_state = False   
+        self.selected_light_period = timedelta(milliseconds = 1000)
 
     def run(self):
         while True:
@@ -38,83 +60,137 @@ class RackController(Thread):
                     raise TypeError("Expected a list of ports but got: ", type(port), port)
             return ports
 
-    selected_light_period = timedelta(milliseconds = 1000)
     def _light_selected_ports(self):
-        # If nothing is selected, prepare for on cycle right away 
-        if not self.selected_ports:
-            self.light_state = True
+        # Filter out all the selected ports. We don't really care about none selected ports in this function. 
+        every_selected = {port_number:state for port_number, state in self.ports_select_state.items() if state.selected}
+        
+        # If nothing is selected, prepare for on cycle right away so when something 
+        # is selected then we turn on imediatly. 
+        if not every_selected:
+            self.rack_light_state = True
             self.light_last_cycle = datetime.now()
             return
 
         # Check if it's time to cycle on/off
-        if datetime.now() > self.light_last_cycle + type(self).selected_light_period:
-            self.light_state = not self.light_state
+        if datetime.now() > self.light_last_cycle + self.selected_light_period:
+            self.rack_light_state = not self.rack_light_state
             self.light_last_cycle = datetime.now()
         
-        # Finally write the state to all the ports. 
-        for port_number in self.selected_ports:
-            self.ports[port_number].set_light(self.light_state)  
+        # Finally write the state to all the selected ports. 
+        for port_number in every_selected:
+            self.ports[port_number].set_light(self.rack_light_state)  
     
     def _monitor_activity(self):
-        # If nothing is selected, don't bother checking
-        if not self.selected_ports:
+        every_selected = [port_number for port_number, state in self.ports_select_state.items() if state.selected]
+
+        # If nothing is selected just stop doing any more checking. 
+        if not every_selected:
             return
-        
-        for port_number in self.ports:   
+
+        # Go through ports and find activity
+        for port_number, port_state in self.ports_select_state.items():
+            # get the activity on the port. if none thes just skip
+            if self.ports[port_number].activity is not True:
+                continue # just skip the rest
             
-            # if there's no activity i wont bother doing anything more
-            if not self.ports[port_number].activity:
-                continue
-            
-            if port_number in self.selected_ports:
+            if port_state.selected:
                 self._handle_pick(port_number)
             else:
-                self._handle_wrong_pick(port_number)
-
+                self._handle_wrong_pick(port_number)          
 
     def _handle_pick(self,port_number):
         try:
-            args = self.selected_ports[port_number]
+            port_state = self.ports_select_state[port_number]
             box = self.boxes[port_number]
+            port = self.ports[port_number]
             
-            # If the amount is there go ahead and remove it from inventory 
-            if box.content_count >= args["amount"]:
-                box.rm_items(args["amount"])
-            else:
-                raise ValueError("Insufficient amount of items in box")
+            # Deselect the port  #TODO use deselect function instead 
+            port_state.selected = False
             
-            # Should we just automatically deselct? 
-            if args["auto_deselect"]:
-                del self.selected_ports[port_number]
+            # Set the timestamp for this pick
+            port_state.pick_timestamp = datetime.now()
 
+            # If auto light off is set then we turn off the light.
+            if port_state.auto_light_off:
+                port.set_light(False) # Turn off the light 
+
+            # If the amount is there go ahead and remove it from inventory 
+            if box.content_count >= port_state.amount:
+                box.rm_items(port_state.amount)
+            else:
+                raise ValueError("Insufficient amount of items in box. Amount must somehow have changed since box was selected. This should normally not happen")
+            
             # If we got a callback go ahead and call it 
-            if args["callback"]:
-                args["callback"](port_number)
+            if port_state.callback is not None:
+                port_state.callback(port_number)
+                
         except KeyError as e:
             raise KeyError("Port {} not found. Must have been removed while selected.".format(port_number), e)
 
 
     def _handle_wrong_pick(self, port_number):
-        print("wrong pick", port_number)
-
-
-    def select_port(self, port_number, amount = 1, callback = None, auto_deselect = True):
+        # If no box is attached just ignore the sensor
+        if not self.boxes.get(port_number):
+            return 
+        
         port = self.ports.get(port_number)
+        port_state = self.ports_select_state.get(port_number)
+
+        
+        time_since_pick = datetime.now() - port_state.pick_timestamp
+        if port and time_since_pick < port.cooldown_time:
+            return
+        else:
+            print("wrong pick", port_number)
+        pass
+
+
+    def select_port(self, port_number, amount = 1, callback = None, auto_light_off = True):
+        # Check that the port is on this rack
+        if port_number not in self.ports:
+            raise KeyError("the port {} is not in this rack".format(port_number))
+
+        # get the box on this port    
         box = self.boxes.get(port_number)
+
+        if not box:
+            raise ValueError("No box attached to this port.")
+
         #check if the box is there and that there's enough content. 
-        if port and box and box.content_count >= amount: 
-            kwargs = {"amount" : amount, "callback": callback, "auto_deselect": auto_deselect}
-            # add port to the list of selected ports indexed by the port number. 
-            self.selected_ports[port.port_number] = kwargs
-            return True
+        if box.content_count >= amount: 
+            kwargs = {"selected": True,
+                      "amount" : amount, 
+                      "callback": callback, 
+                      "auto_light_off": auto_light_off,
+                      "select_timestamp": datetime.now()}
+            # Update port status
+            self.ports_select_state[port_number].update(**kwargs)
+
         else:
             return False
+    
+    def deselect_port(self, port_number, auto_light_off = True):
+        # if port not in this rack return false
+        if port_number not in self.ports:
+            return False
+        
+        if port_number not in self.ports_select_state:
+            return True
+        
+        if auto_light_off or self.ports_select_state:
+            port = self.ports.get(port_number)
+            port.set_light(False) # Turn off the light
 
-    def select_content_id(self, content_id, amount = 1, callback = None, auto_deselect = True):
+        # remove the selected port
+        del self.ports_select_state[port_number]
+
+        return True
+
+    def select_content_id(self, content_id, amount = 1, callback = None, auto_light_off = True):
         # TODO find the box on the rack with the given content id
         pass
 
-    def select_box_id(self, box_id, amount = 1, callback = None, auto_deselect = True):
+    def select_box_id(self, box_id, amount = 1, callback = None, auto_light_off = True):
         # TODO find the port that has this box_id
         pass
     
