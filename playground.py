1aac9fbce681023b406d4f0888cdde94259c1008
Hjalte Nielsen <hjalte33@gmail.com>
2020-03-13 16:51:35 +0100

    wha

--- playground.py
+++ playground.py
@@ -12,7 +12,7 @@ class Dummy:
         self.s2 = "davdav"
 
     def wow(self):
-        print(self.testvar2)
+        print(self.s1)
 
 
 #i = Dummy()
@@ -21,23 +21,50 @@ class Dummy:
 
 ################################################################
 
-s = Dummy()
-l = Dummy()
+import curses
+import time
+import threading
 
-a_list = {"one": s, "two": l}
+def show_progress(win,X_line,sleeping_time):
 
-b_list = []
-b_list.append(a_list["two"])
+    # This is to move the progress bar per iteration.
+    pos = 10
+    # Random number I chose for demonstration.
+    for i in range(15):
+        # Add '.' for each iteration.
+        win.addstr(X_line,pos,".")
+        # Refresh or we'll never see it.
+        win.refresh()
+        # Here is where you can customize for data/percentage.
+        time.sleep(sleeping_time)
+        # Need to move up or we'll just redraw the same cell!
+        pos += 1
+    # Current text: Progress ............... Done!
+    win.addstr(X_line,26,"Done!")
+    # Gotta show our changes.
+    win.refresh()
+    # Without this the bar fades too quickly for this example.
+    time.sleep(0.5)
 
-b_list[0].s2 = "hejhej"
+def show_progress_A(win):
+    show_progress( win, 1, 0.1)
 
-del a_list["two"]
+def show_progress_B(win):
+    show_progress( win, 4 , 0.5)
+
+if __name__ == '__main__':
+    curses.initscr()
 
 
-print (a_list["one"].s2, b_list[0].s2)
+    win = curses.newwin(6,32,14,10)
+    win.border(0)
+    win.addstr(1,1,"Progress ")
+    win.addstr(4,1,"Progress ")
+    win.refresh()
 
-
-sleep(1)
+    threading.Thread( target = show_progress_B, args = (win,) ).start()
+    time.sleep(2.0)
+    threading.Thread( target = show_progress_A, args = (win,)).start()
 
 
 
