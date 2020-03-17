dc46d0dd8964f1f2e420550ccac5d6bdf3ef38d0
Hjalte Nielsen <hjalte33@gmail.com>
2020-03-17 10:24:43 +0100

    cleanout old files

--- simple_pick_by_light/setup.py
+++
@@ -1,5 +0,0 @@
-setup(...,
-      packages=['simple_pick_by_light'],
-      package_dir={'simple_pick_by_light': 'src/simple_pick_by_light'},
-      package_data={'simple_pick_by_light': ['data/*.yaml']},
-      )
